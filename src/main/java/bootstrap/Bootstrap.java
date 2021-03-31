41936e6d5e003efa52de7d693a0ba7bfee49d6d1
acandiatale <acandiatale@gmail.com>
2021-03-31 20:27:46 +0900

    bootstrap class modify

--- src/main/java/bootstrap/Bootstrap.java
+++ src/main/java/bootstrap/Bootstrap.java
@@ -1,13 +1,8 @@
 package bootstrap;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.eclipse.jetty.server.Connector;
-import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
-import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
 
 public class Bootstrap {
@@ -24,23 +19,5 @@ public class Bootstrap {
 		// Add the Connector to the Server
 		server.addConnector(connector);
 
-		// Set a simple Handler to handle requests/responses.
-		server.setHandler(new AbstractHandler()
-		{
-		    @Override
-		    public void handle(String target, Request jettyRequest, HttpServletRequest request, HttpServletResponse response)
-		    {
-		        // Mark the request as handled so that it
-		        // will not be processed by other handlers.
-		        jettyRequest.setHandled(true);
-		    }
-		});
-
-		// Start the Server so it starts accepting connections from clients.
-		try {
-			server.start();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 	}
 }

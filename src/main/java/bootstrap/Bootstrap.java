e45659f8d2b48dce9601aba85868b703631dd6a9
acandiatale <acandiatale@gmail.com>
2021-04-20 07:20:49 +0900

    modify rewrite URL

--- src/main/java/bootstrap/Bootstrap.java
+++ src/main/java/bootstrap/Bootstrap.java
@@ -2,11 +2,14 @@ package bootstrap;
 
 import core.PropertyUtils;
 import dbFactory.DataAccess;
+import httpAdapter.HttpServerFactory;
 
 public class Bootstrap {
 	public static void main(String[] args) {
 		PropertyUtils.getInstance().init();
 		DataAccess db = new DataAccess();
 		db.init();
+		HttpServerFactory server = new HttpServerFactory();
+		server.init();
 	}
 }
--- src/main/java/handlerFactory/HandlerFactory.java
+++ src/main/java/handlerFactory/HandlerFactory.java
@@ -4,18 +4,26 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URI;
 
+import org.eclipse.jetty.rewrite.RewriteCustomizer;
 import org.eclipse.jetty.rewrite.handler.CompactPathRule;
+import org.eclipse.jetty.rewrite.handler.RedirectRegexRule;
 import org.eclipse.jetty.rewrite.handler.RewriteHandler;
 import org.eclipse.jetty.rewrite.handler.RewriteRegexRule;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.server.handler.DefaultHandler;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ErrorPageErrorHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.resource.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import servletManager.ErrorPageServlet;
 import servletManager.TestServlet;
 
 public class HandlerFactory {
@@ -45,22 +53,36 @@ public class HandlerFactory {
 		HandlerList handlerList = new HandlerList();
 		
 		RewriteHandler rewrite = new RewriteHandler();
+		rewrite.setRewriteRequestURI(true);		
+		rewrite.setRewritePathInfo(false);
+		rewrite.setOriginalPathAttribute("requestedPath");
 		
-		rewrite.addRule(new CompactPathRule());
-		rewrite.addRule(new RewriteRegexRule("(\\/.*)", "/index.html"));
+//		rewrite.addRule(new CompactPathRule());
+		rewrite.addRule(new RewriteRegexRule("(\\/introduce)?", "/index.html"));
 		
-		ContextHandlerCollection contextCollection = new ContextHandlerCollection();
-		rewrite.setHandler(contextCollection);
+//		rewrite.addRule(new RewriteRegexRule("(\\/.*)", "/index.html"));
 		
-		ResourceHandler handler = new ResourceHandler();
+		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+		
 		try {
-			handler.setBaseResource(Resource.newResource(uri));
-			handler.setDirectoriesListed(false);
-			handler.setWelcomeFiles(new String[] {"index.html"});
-			handler.setAcceptRanges(true);
-			handlerList.addHandler(handler);
-			contextCollection.addHandler(handlerList);
-			server.setHandler(rewrite);
+			context.setContextPath("/");
+			context.setWelcomeFiles(new String[] {"index.html"});
+			context.setBaseResource(Resource.newResource(uri));
+			context.addServlet(ErrorPageServlet.class, "/errorpage");
+			ErrorPageErrorHandler errorMapper = new ErrorPageErrorHandler();
+			errorMapper.addErrorPage(404, "/errorpage");
+			context.setErrorHandler(errorMapper);
+			rewrite.setHandler(context);
+			
+			ServletHolder defHolder = new ServletHolder("default", DefaultServlet.class);
+			defHolder.setInitParameter("dirAllowed", "false");
+			defHolder.setAsyncSupported(true);
+//			defHolder.setInitParameter("resourceBase", "dist/");
+			context.addServlet(defHolder, "/");
+			
+			DefaultHandler def = new DefaultHandler();
+			handlerList.setHandlers(new Handler[] {rewrite, def});
+			server.setHandler(handlerList);
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
